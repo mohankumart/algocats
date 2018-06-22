f32396379f3eccc1d07201ac5727852bde5e7c68
Mohan Kumar <tmk_mohankumar@yahoo.com>
2018-06-22 23:13:28 +0530

    add methods to reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,65 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//     var length = str.length;
+//     var reverseString = '';
+//     for(var i=length-1; i>=0; i--){
+//         reverseString += str.charAt(i);
+//     }
+//     return reverseString;
+// }
 
-module.exports = reverse;
+// function reverse(str){
+//     let reversed = '';
+
+//     for(let character of str){
+//         reversed = character + reversed;
+//     }
+
+//     return reversed;
+
+// }
+
+function reverse(str){
+    return str.split('').reduce((initialValue, currentValue) => currentValue + initialValue,'');
+}
+
+function sum(arr){
+    return arr.reduce((accumulate, arrayValue) => accumulate+arrayValue);
+}
+
+function sumOfValuesInObject(objArr){
+    let initialValue = 0;
+    return objArr.reduce((accumulate, currentObject) => accumulate + currentObject.x, initialValue);
+}
+
+function flattenArray(arrArr){
+    return arrArr.reduce((accumulate, currentItem) => accumulate.concat(currentItem));
+}
+
+function countInstances(arr){
+    return arr.reduce((accumulate, currentValue) => {
+        if(currentValue in accumulate){
+            accumulate[currentValue]++;
+        }else{
+            accumulate[currentValue] = 1;
+        }
+        return accumulate;
+    },{});
+}
+
+function groupByProperty(arrObject, property){
+    return arrObject.reduce((accumulate, currentObject)=>{
+        if(currentObject[property] in accumulate){
+            accumulate[currentObject[property]].push(currentObject);
+        }else{
+            accumulate[currentObject[property]] = [currentObject];
+        }
+        return accumulate;
+    },{});
+}
+
+console.log(groupByProperty([{name:'mohan',age:20},{name:'kumar',age:20},{name:'mohan',age:33}], 'age'));
+
+module.exports = {reverse, sum, sumOfValuesInObject, flattenArray, countInstances};
--- exercises/reversestring/test.js
+++ exercises/reversestring/test.js
@@ -1,4 +1,8 @@
-const reverse = require('./index');
+const reverse = require('./index').reverse;
+const sum = require('./index').sum;
+const sumOfValuesInObject = require('./index').sumOfValuesInObject;
+const flattenArray = require('./index').flattenArray;
+const countInstances = require('./index').countInstances;
 
 test('Reverse function exists', () => {
   expect(reverse).toBeDefined();
@@ -10,4 +14,24 @@ test('Reverse reverses a string', () => {
 
 test('Reverse reverses a string', () => {
   expect(reverse('  abcd')).toEqual('dcba  ');
+});
+
+test('sum function exists', () => {
+  expect(sum).toBeDefined();
+});
+
+test('Sum of an array', () => {
+  expect(sum([1,2,3])).toEqual(6);
+});
+
+test('sum of values in object array', () => {
+  expect(sumOfValuesInObject([{x:1},{x:2},{x:3}])).toEqual(6);
+});
+
+test('flatten an array', () => {
+  expect(flattenArray([[1,2],[3,4],[5,6]])).toEqual([1,2,3,4,5,6]);
+});
+
+test('count instances', () => {
+  expect(countInstances(['mohan','kumar','mohan'])).toEqual({'mohan':2, 'kumar':1});
 });
