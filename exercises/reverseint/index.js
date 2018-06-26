a9590545c2bac9b8fe3a99275b8901f25f671b6e
Mohan Kumar <tmk_mohankumar@yahoo.com>
2018-06-26 15:28:16 +0530

    reverseInt changes

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,13 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let revesedN = parseInt(n.toString().split('').reduce((accumulate, currEle)=>accumulate = currEle + accumulate,''));
+    if(n < 0){
+        return revesedN * -1;
+    }
+    return revesedN;
+}
 
+console.log(reverseInt(-51));
 module.exports = reverseInt;
