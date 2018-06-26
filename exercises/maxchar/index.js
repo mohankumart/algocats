4bc4c36592d563b8f81a5da56621e0783e39fce2
Mohan Kumar <tmk_mohankumar@yahoo.com>
2018-06-26 20:09:04 +0530

    maxchar problem

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let accumulateObj = {};
+    for(let char of str){
+        accumulateObj[char] = accumulateObj[char] + 1 || 1;
+    }
+
+    let max = 0, maxKey;
+    for(key in accumulateObj){
+        if(accumulateObj[key] > max){
+            max = accumulateObj[key];
+            maxKey = key;
+        }
+    }
+
+    return maxKey;
+}
 
 module.exports = maxChar;
