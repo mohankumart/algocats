d277befb20160f70f4dac5c7ae981063caf4ab84
Mohan Kumar <tmk_mohankumar@yahoo.com>
2018-06-28 12:59:59 +0530

    new algos

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,51 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// function chunk(array, size) {
+//     let tempSize = size;
+//     let resultArray = [];
+//     let temArray = [];
 
+//     for(let i=0; i<=array.length; i++){
+//         if(tempSize == 0 || i == array.length){
+//             resultArray.push(temArray.slice());
+//             tempSize = size;
+//             temArray.length = 0;
+//         }
+//         if(i != array.length){
+//             temArray.push(array[i]);
+//             tempSize--;
+//         }
+//     }
+//     return resultArray;
+// }
+
+// function chunk(array, size){
+//     let resultArray = [];
+//     for(let char of array){
+//         let lastEle = resultArray[resultArray.length - 1];
+//         if(lastEle){
+//             if(lastEle.length < size){
+//                 lastEle.push(char);
+//             }else{
+//                 resultArray.push([char]);
+//             }
+//         }else{
+//             resultArray.push([char]);
+//         }
+//     }
+//     return resultArray;
+// }
+
+function chunk(array, size){
+    let resultArray = [];
+    let index = 0;
+    while(index < array.length){
+        resultArray.push(array.slice(index, index+size));
+        index += size;
+    }
+    return resultArray;
+}
+
+console.log(chunk([1,2,3], 2));
 module.exports = chunk;
