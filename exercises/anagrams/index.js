daa6b4c06466676ae8d42faad93617d4f5bf7398
Mohan Kumar <tmk_mohankumar@yahoo.com>
2018-06-29 14:35:01 +0530

    problems

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,50 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// function anagrams(stringA, stringB) {
+//     const stringMapA = generateMap(stringA);
+//     const stringMapB = generateMap(stringB);
+    
+//     if(Object.keys(stringMapA).length != Object.keys(stringMapB).length){
+//         return false;
+//     }
+
+//     for(key in stringMapA){
+//         if(stringMapA[key] != stringMapB[key]){
+//             return false;
+//         }
+//     }
+    
+//     return true;
+// }
+
+// function generateMap(str){
+//     const strMap = {};
+//     for(char of str.replace(/[^a-zA-Z0-9]/g, str)){
+//         strMap[char] = strMap[char] + 1 || 1;
+//     }
+//     return strMap;
+// }
+
+// function anagrams(stringA, stringB){
+//     let parsedStringA = stringA.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+//     let parsedStringB = stringB.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    
+//     for(let char of parsedStringA){
+//         parsedStringB = parsedStringB.replace(char, "");
+//     }
+
+//     if(parsedStringB.length == 0){
+//         return true;
+//     }else{
+//         return false;caches
+//     }
+// }
+
+function anagrams(stringA, stringB){
+
+}
+
+anagrams('hello', 'llohe');
 
 module.exports = anagrams;
