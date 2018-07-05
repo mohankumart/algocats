5867e4c830fa554039702cba5daa5666718574e5
Mohan Kumar <tmk_mohankumar@yahoo.com>
2018-07-05 15:04:58 +0530

    changes

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,21 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+//     let vowelCount = 0;
+//     const checker = ['a','e','i','o','u'];
+//     for(let char of str.toLowerCase()){
+//         if(checker.includes(char)){
+//             vowelCount++;
+//         }
+//     }
+//     return vowelCount;
+// }
+
+function vowels(str){
+    const regex = new RegExp('[aeiou]','gi');
+    const matches = str.match(regex);
+    return matches ? matches.length : 0
+}
 
 module.exports = vowels;
