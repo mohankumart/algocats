97fb4ff876a6cdfa00cf1955b3b0f8e4d9f6f1ac
Mohan Kumar <tmk_mohankumar@yahoo.com>
2018-07-16 15:26:32 +0530

    circular list changes

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,6 +12,35 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+const linkedList = require('./linkedlist').LinkedList;
+const Node = require('./linkedlist').Node;
+
+function circular(list) {
+    let t1 = list.getFirst();
+    let t2 = list.getFirst();
+debugger;
+    while(t2.next && t2.next.next){
+        t2 = t2.next.next;
+        t1 = t1.next;
+
+        if(t1 == t2){
+            return true;
+        }
+    }
+
+    return false;
+
+}
+
+  const l = new linkedList();
+  const a = new Node('a');
+  const b = new Node('b');
+  const c = new Node('c');
+  l.head = a;
+  a.next = b;
+  b.next = c;
+  c.next = null;
+  console.log(circular(l)) // true
+
 
 module.exports = circular;
